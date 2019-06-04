36237f02c4872a9c0943304c79191064d435b00d
VikalpP <vikalppanseriya@gmail.com>
2019-06-04 23:32:57 +0530

    bug: change server url

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,6 +1,6 @@
-const SERVER_URL = "http://localhost:3001/";
-let USER_ID = localStorage.getItem("userId")
-  ? localStorage.getItem("userId").toString()
-  : ""; //"5cf57ee8f619f130977dd3c9";
-const serverUrl = "10.173.1.206:3001";
+const SERVER_URL = 'http://localhost:3001/';
+let USER_ID = localStorage.getItem('userId')
+  ? localStorage.getItem('userId').toString()
+  : ''; //"5cf57ee8f619f130977dd3c9";
+const serverUrl = 'http://10.173.7.42:3001/';
 export { SERVER_URL, USER_ID, serverUrl };
